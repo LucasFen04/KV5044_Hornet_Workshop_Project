aa4edb23b6d97eb72a9efdc212c34957dac8f6d7
LucasFen04 <w23022188@northumbria.ac.uk>
2025-02-20 12:43:00 +0000

    Changes made, ship now moves correctly, must finish at home

--- Hornet/Spaceship.cpp
+++ Hornet/Spaceship.cpp
@@ -5,22 +5,23 @@ Spaceship::Spaceship() : GameObject(ObjectType::SPACESHIP)
 }
 void Spaceship::Update(double frametime)
 {
-    if (HtKeyboard::instance.KeyPressed(SDL_SCANCODE_LEFT));
-    m_angle = m_angle - 0.01;
-
-    if (HtKeyboard::instance.KeyPressed(SDL_SCANCODE_RIGHT));
-    m_angle = m_angle + 0.01;
-
-    Vector2D shipAcceleration;
+    Vector2D shipAcceleration (0, 0.01);
 
 
     if (HtKeyboard::instance.KeyPressed(SDL_SCANCODE_UP))
     {
         m_velocity.setBearing(m_angle, 0.1);
-        m_velocity = m_velocity + shipAcceleration;
-        m_position = m_position + m_velocity;
+        m_velocity = m_velocity + shipAcceleration * frametime;
     }
-
+    if (HtKeyboard::instance.KeyPressed(SDL_SCANCODE_RIGHT))
+    {
+        m_angle = m_angle + 0.02;
+    }
+    if (HtKeyboard::instance.KeyPressed(SDL_SCANCODE_LEFT))
+    {
+        m_angle = m_angle - 0.02;
+    }
+    m_position = m_position + m_velocity;
 }
 void Spaceship::Initialise()
 {
